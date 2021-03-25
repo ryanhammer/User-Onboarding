dfd13b2c85b97b929a2fe79a09b25671b718a7ba
rhammer35 <rhammer35@icloud.com>
2021-03-25 19:29:49 -0400

    created test to make sure form was submittable

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -9,8 +9,10 @@ describe('User Onboarding App', () => {
   const nameInput = () => cy.get('input[name="name"]');
   const emailInput = () => cy.get('input[name="email"]');
   const passwordInput = () => cy.get('input[name="password"]');
-  const termsBox = () => cy.get('input[name="terms"]');
+  const roleSelect = () => cy.get('select[name="role"]');
+  const houseInput = () => cy.get('input[name="house"]');
   const submitBtn = () => cy.get('#submitBtn');
+  const termsBox = () => cy.get('input[name="terms"]');
 
   it("allows user to type in the name, email, and password inputs", () => {
     // grab the inputs, assert they are empty, type in them, and assert that what we typed is there
@@ -37,6 +39,38 @@ describe('User Onboarding App', () => {
       .click()
       .should('be.checked');
   });
+
+  it("allows user to submit form once input requirements are met", () => {
+    // start with empty form and disabled submit button
+    nameInput().should('have.value', '');
+    emailInput().should('have.value', '');
+    passwordInput().should('have.value', '');
+    roleSelect().should('have.value', '');
+    houseInput().should('not.be.checked');
+    termsBox().should('not.be.checked');
+    submitBtn().should('be.disabled');
+
+    // correctly fill in form fields that pass validation
+    nameInput().type('Albus Dumbledore');
+    emailInput().type('FawkesDad@hogwarts.com');
+    passwordInput().type('Lemon Drop');
+    roleSelect().select('Headmaster');
+    houseInput().check('Gryffindor');
+    termsBox().click();
+
+    // check that submit button is enabled, click it, and verify form clears upon submission
+    submitBtn()
+      .should('not.be.disabled')
+      .click();
+    nameInput().should('have.value', '');
+    emailInput().should('have.value', '');
+    passwordInput().should('have.value', '');
+    roleSelect().should('have.value', '');
+    houseInput().should('not.be.checked');
+    termsBox().should('not.be.checked');
+    submitBtn().should('be.disabled');
+
+  })
 });
 
 // it('submit button is disabled until both inputs are filled out', () => {
